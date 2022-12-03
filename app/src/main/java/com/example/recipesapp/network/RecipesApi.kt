ffa5715b319a128c6fc8982471893615e4916812
Cristina-Maria Miulescu <cristinamaria.miulescu@gmail.com>
2022-12-03 20:28:51 +0200

    Setup RecipesApi Interface to make requests

---
+++ app/src/main/java/com/example/recipesapp/network/RecipesApi.kt
@@ -0,0 +1,18 @@
+package com.example.recipesapp.network
+
+import com.example.recipesapp.model.RecipesModel
+import com.example.recipesapp.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface RecipesApi {
+
+    @GET(value = "recipes/random")
+    suspend fun getRandomRecipes(
+        @Query("limitLicense") query: Boolean = true,
+        @Query("number") number : Int = 2,
+        @Query("apiKey") apiKey: String = Constants.API_KEY
+    ): RecipesModel
+}
